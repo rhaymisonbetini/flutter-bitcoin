5096618f0a5cda1e9ada3ed2be8e65f0b8bf1a8b
RHAYMISON <rhaymison@hotmail.com>
2021-03-09 09:07:49 -0300

    first commit

---
+++ android/app/src/main/java/com/myfirstfluter/flutter_bitcoin_price/MainActivity.java
@@ -0,0 +1,6 @@
+package com.myfirstfluter.flutter_bitcoin_price;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bitcoin_price/Home.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
